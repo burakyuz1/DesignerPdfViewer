9bd4de4ce21c06219bd853b2a51559450a811aa3
Burak Akyüz <burakyuzz@gmail.com>
2021-10-23 13:36:28 +0300

    Add project files.

---
+++ deneme/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace deneme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] h = { 1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 7 };
+
+            string word = Console.ReadLine();
+
+            int result = designerPdfViewer(h, word);
+
+            Console.WriteLine(result);
+
+
+            Console.ReadKey();
+        }
+
+        public static int designerPdfViewer(int[] h, string word)
+        {
+            int max = 0;
+            int[] asciisOfWord = new int[word.Length];
+            for (int i = 0; i < word.Length; i++)
+            {
+                asciisOfWord[i] = word[i] - 97;
+            }
+            for (int i = 0; i < asciisOfWord.Length; i++)
+            {
+                if (h[asciisOfWord[i]] > max)
+                    max = h[asciisOfWord[i]];
+            }
+
+            return max * word.Length;
+        }
+
+    }
+}
